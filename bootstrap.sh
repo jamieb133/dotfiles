3db1254ef4f192ef9e2759f22e77cebab818d19a
= <=>
2025-05-18 12:17:53 +0100

    Add bootstrap.sh to download homebrew, install brew packages, clone the repo and install

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# ONLY RUN THIS ONCE VIA CURL TO INSTALL BREW STUFF AND CLONE THE REPO
+
+# Do this within the HOME directory
+cd $HOME
+
+if command -v brew &> /dev/null
+then
+    echo "Homebrew is installed."
+    brew --version
+else
+    echo "Homebrew is not installed, installing now."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Download the list of package dependencies and install them
+curl -O https://raw.githubusercontent.com/jamieb133/dotfiles/refs/heads/main/Brewfile
+brew bundle
+rm Brewfile
+
+# Download and install the stuff
+git clone git@github.com:jamieb133/dotfiles.git
+cd dotfiles
+git submodule update --init
+sh install.sh
+
